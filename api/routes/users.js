7a86dc194db43ec59f9b54c3b4a29fc0a2c44323
Meenusiwach <123761808+Meenusiwach@users.noreply.github.com>
2024-06-18 18:53:13 +0530

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -22,7 +22,7 @@ const router = express.Router();
 //UPDATE the user data
 router.put("/:id", verifyUser, updateUser);
 
-//DELETE
+//DELETE USER DETAILS
 router.delete("/:id", verifyUser, deleteUser);
 
 //GET
